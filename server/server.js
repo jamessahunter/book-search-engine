917709519884d79b79123f38b61d643cd58ea7d6
James Hunter <jamessahunter@gmail.com>
2023-12-06 17:14:23 -0800

    debug

--- server/server.js
+++ server/server.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const path = require('path');
-const { ApolloServer } = require('@apollo/server')
+const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
-const db = require('./config/connection');
-// const routes = require('./routes');
+const path = require('path');
 const { typeDefs, resolvers } = require('./schemas');
-// const { Server } = require('http');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
 
 const startApolloServer = async () => {
-  await Server.start();
+  await server.start();
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use('graphql',expressMiddleware(server))
+  app.use('/graphql',expressMiddleware(server))
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-// app.use(routes);
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
 
 db.once('open', () => {
   app.listen(PORT, () => {
